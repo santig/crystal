0bbb0d4a45d6a4bca5fdf4f0e23c16156000dd9d
Juan Pemberthy <jpemberthy@gmail.com>
2014-01-21 17:21:07 -0800

    moar debug :(

--- lib/adapters.rb
+++ lib/adapters.rb
@@ -2,6 +2,7 @@ module Adapters
   class << self
 
     def process(event, options = Hashie::Mash.new)
+      puts "adapters processing #{event}"
       listeners[event.to_sym].each do |adapter|
         adapter.new(options).send("process_#{event}")
       end
