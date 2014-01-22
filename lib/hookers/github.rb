b9161066477f3f0b5a65f2964c563f6d41a93cfc
Juan Pemberthy <jpemberthy@gmail.com>
2014-01-21 17:18:15 -0800

    Do not puts all that shit.

--- lib/hookers/github.rb
+++ lib/hookers/github.rb
@@ -1,7 +1,6 @@
 module Hookers
   class Github < Sinatra::Base
     post '/github' do
-      puts request.env.inspect
       event = request.env["HTTP_X_GITHUB_EVENT"].to_sym
       payload = Hashie::Mash.new(JSON.parse(params[:payload] || "{}"))
       Adapters.process(event, payload)
