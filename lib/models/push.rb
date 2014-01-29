3303bcce05c75b1477c36139207d7ca7f0f73e2e
Juan Pemberthy <jpemberthy@gmail.com>
2014-01-29 12:59:19 -0800

    one more time.

--- lib/models/push.rb
+++ lib/models/push.rb
@@ -2,40 +2,40 @@ class Push
   attr_reader :action, :options
 
   def initialize(options)
+    puts "push options #{options.inspect}"
     @action = options.action
     @options = options
     @pusher = options.pusher.username
   end
 
   def branch_name
-    options.ref.match(/\/(.*)$/)[1]
+    options.ref ? options.ref.match(/\/(.*)$/)[1] : ""
   end
-  
+
   def base_ref_name
-    options.base_ref.match(/\/(.*)$/)[1]
+    options.base_ref ? options.base_ref.match(/\/(.*)$/)[1] : ""
   end
-  
+
   def branch_names
     "#{branch_name} #{base_ref_name}"
   end
-  
+
   def branch_url
-    puts options.compare
     options.compare.sub(/\/compare\//,"/tree/")
   end
-  
+
   def changes_url
     options.compare
   end
-  
+
   def feature_branch?
     !!options.ref.match(/feature\/.*$/)
   end
-  
+
   def feature_merge?
     !!(base_ref_name.match(/feature\/.*$/) && branch_name.match(/\/develop$/))
   end
-  
+
   def new_branch?
     !!options.created
   end
