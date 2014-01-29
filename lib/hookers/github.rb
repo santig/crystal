9a633420ea39d94348f38cafc29d650b1b6d68cf
Juan Pemberthy <jpemberthy@gmail.com>
2014-01-29 15:31:54 -0800

    the hooker is now receiving content-type application/json instead of a form post

--- lib/hookers/github.rb
+++ lib/hookers/github.rb
@@ -2,7 +2,7 @@ module Hookers
   class Github < Sinatra::Base
     post '/github' do
       event = request.env["HTTP_X_GITHUB_EVENT"].to_sym
-      payload = Hashie::Mash.new(JSON.parse(params[:payload] || "{}"))
+      payload = Hashie::Mash.new(JSON.parse(request.body.string))
       Adapters.process(event, payload)
       "Ok"
     end
